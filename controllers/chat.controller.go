e4e9377553e5eb6f660d8b9ade4e6744c8115ec0
calm428 <ollie21428@gmail.com>
2024-07-30 14:30:17 +0400

    mode for push in chat url

--- controllers/chat.controller.go
+++ controllers/chat.controller.go
@@ -129,7 +129,7 @@ func CreateChatRoomForDM(c *fiber.Ctx) error {
 		}
 
 		roomIDStr := strconv.FormatUint(newRoom.ID, 10)
-		pageURL := fmt.Sprintf("https://www.myru.online/ru/chat/%s", roomIDStr)
+		pageURL := fmt.Sprintf("https://www.myru.online/ru/chat/%s?mode=false", roomIDStr)
 
 		// sendPushNotificationToOwner(acceptorUser.ID, requestorUser.Name, initialMessage.Content, pageURL)
 		sendNotificationToOwner(acceptorUser.ID.String(), requestorUser.Name, initialMessage.Content, pageURL)
@@ -667,7 +667,7 @@ func SendMessageForDM(c *fiber.Ctx) error {
 	}
 
 	roomIDStr := strconv.FormatUint(message.RoomID, 10)
-	pageURL := fmt.Sprintf("https://www.myru.online/chat/%s", roomIDStr)
+	pageURL := fmt.Sprintf("https://www.myru.online/chat/%s?mode=false", roomIDStr)
 
 	// sendPushNotificationToOwner(recipient.UserID, user.Name, message.Content, pageURL)
 	sendNotificationToOwner(recipient.UserID.String(), user.Name, message.Content, pageURL)
